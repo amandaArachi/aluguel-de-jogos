436da8bebd5904c39e1df6998816975339538895
amandaArachi <amanda.arachi@hotmail.com>
2025-06-21 14:27:58 -0300

    botão alugar/devolver

--- js/app.js
+++ js/app.js
@@ -5,9 +5,13 @@ function alterarStatus(id) {
 
         if(imagem.classList.contains('dashboard__item__img--rented')) {
             imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
 
         } else {
             imagem.classList.add('dashboard__item__img--rented'); 
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
         }
 }
 
