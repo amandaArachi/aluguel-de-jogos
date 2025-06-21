b1283c5d64ae8d53e88645cbaefb06020f7719fb
amandaArachi <amanda.arachi@hotmail.com>
2025-06-21 14:58:55 -0300

    confirmar devolução 1.0

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@ function alterarStatus(id) {
     let jogoClicado = document.getElementById(`game-${id}`);
     let botao = jogoClicado.querySelector('.dashboard__item__button');
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let nomeJogo= jogoClicado.querySelector('.dashboard__item__name').textContent;
 
         if(imagem.classList.contains('dashboard__item__img--rented')) {
             imagem.classList.remove('dashboard__item__img--rented');
             botao.classList.remove('dashboard__item__button--return');
             botao.textContent = 'Alugar';
-            alert('ATENÇÃO: Você possui este ítem, deseja devolvê-lo? Esta ação não terá volta!');
+            alert(`ATENÇÃO: Você possui ${nomeJogo}, deseja devolvê-lo? Esta ação não terá volta!`);
 
         } else {
             imagem.classList.add('dashboard__item__img--rented'); 
