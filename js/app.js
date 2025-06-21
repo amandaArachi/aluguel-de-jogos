3ceaa0a82348e3283e1c7550605d1eace1c89ef7
amandaArachi <amanda.arachi@hotmail.com>
2025-06-21 14:23:03 -0300

    imagem opaca

--- js/app.js
+++ js/app.js
@@ -2,4 +2,12 @@ function alterarStatus(id) {
     let jogoClicado = document.getElementById(`game-${id}`);
     let botao = jogoClicado.querySelector('.dashboard__item__button');
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
+
+        if(imagem.classList.contains('dashboard__item__img--rented')) {
+            imagem.classList.remove('dashboard__item__img--rented');
+
+        } else {
+            imagem.classList.add('dashboard__item__img--rented'); 
+        }
 }
+
