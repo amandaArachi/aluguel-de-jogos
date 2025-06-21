5481e146c9f0bc489ae7c05ad864472ba9dde045
amandaArachi <amanda.arachi@hotmail.com>
2025-06-20 21:20:26 -0300

    botão clicável

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function alterarStatus(id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+}
